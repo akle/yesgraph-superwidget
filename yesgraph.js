52ccd2b7bf05df812a0851e9003ca73c93298a91
Carolyn <carolyn.lee@yale.edu>
2016-01-25 16:42:41 -0800

    Add contact importing, ranking, searching, selecting, and mailto

--- yesgraph.js
+++ yesgraph.js
@@ -1,18 +1,18 @@
 (function($){
+    var appName,
+        YESGRAPH_BASE_URL = 'http://localhost:5001';  // FIXME
 
-    function getGoogleAuthUrl() {
-
-        var oAuthClientID = "1087547563887-dc6k04gv92q59l404vfu43f0u8s0u43e.apps.googleusercontent.com";
-        var oAuthRedirectURL = "http://localhost:5001";
-
+    function getGoogleAuthUrl(options) {
         var requestParams = {
             response_type: "token",
-            client_id: oAuthClientID,
-            redirect_uri: oAuthRedirectURL,
-            scope: "https://www.googleapis.com/auth/contacts.readonly",
+            client_id: options.integrations.google.clientId,
+            // TODO: if this is to be embedded on multiple pages,
+            // we need to be able to use distinct redirect URLs
+            redirect_uri: options.redirectUrl,
+            scope: "https://www.google.com/m8/feeds/" // for getting contacts
         }
         var oAuthURL =  "https://accounts.google.com/o/oauth2/auth?" + $.param(requestParams);
-                return oAuthURL;
+        return oAuthURL;
     }
 
     function getUrlParams () {
@@ -55,35 +55,171 @@
         };
 
         var contacts = {
-            client_token: '',
+            client_token: '',  // updated when we displayRankedContacts
             source: {
                 type: 'gmail',
-                name: contactsFeed.author.name,
-                email: contactsFeed.author.email
+                name: contactsFeed.author[0].name.$t,
+                email: contactsFeed.author[0].email.$t
             },
             entries: entries
         }
+        return contacts;
+    }
+
+    function renderContacts(rankedContacts, $dialog) {
+        var entry,
+            $contactRow,
+            $contactsList = $('<div>'),
+            $searchField = $('<input>', {placeholder: "Search"}).css({border: "1px solid #ccc", "border-radius": "3px", "padding": "0.5em"}),
+            $sendBtn = $('<button>', {text: "Send"});
+
+        // Load each contact entry onto the list
+
+        for (var i=0; i < rankedContacts.length; i++) {
+            entry = rankedContacts[i];
+            if (entry.emails.length !== 0) {
+
+                $contactRow = $('<div>', {
+                    "class": "contactRow"
+                });
+
+                var contactRowCSS = {
+                    'border-bottom': '1px solid #ccc',
+                    'min-height': '50px',
+                    'padding': '0.5em'
+                };
+
+                if (entry.name) {
+                    $contactRow.append($('<span>', {text: entry.name}).css({'font-weight': 'bolder'}),
+                                       $('<br>'),
+                                       $('<span>', {text: entry.email}));
+                } else {
+                    $contactRow.append($('<span>', {text: entry.email}).css({'font-weight': 'bolder'}));
+                }
+
+                $contactRow.data('email', entry.emails[0]);
+                $contactRow.css(contactRowCSS)
+
+                // Handle hover & click events
+                $contactRow.hover(toggleHighlight);
+                $contactRow.click(toggleSelected);
+
+                $contactsList.append($contactRow);
+            }
+
+        }
+
+        // Setup the Send button
+
+        $sendBtn.css({
+            "border": "1px solid #CCC",
+            "border-radius": "3px",
+            "margin": "5px",
+            "padding": "0.25em 0.5em",
+            "background-color": "#F6F6F6"
+        });
+
+        $sendBtn.on('click', function () {
+            // Open a mailto for the selected names
+            var selectedEmails = getSelectedEmails();
+            window.location.href = 'mailto:' + selectedEmails.join(',');
+        });
+
+        $dialog.append($searchField, $contactsList, $sendBtn);
+        searchable($searchField, $contactsList);
+
+        function toggleHighlight (evt) {
+            var $this = $(this);
+            if (!$this.hasClass('selected')) {
+                if (evt.type === "mouseenter") {
+                    $this.css({'background-color': '#F6F6F6'});
+                } else if (evt.type === "mouseleave") {
+                    $this.css({'background-color': 'white'});
+                }                    
+            }
+        }
+
+        function toggleSelected () {
+            var $this = $(this);
+
+            if ($this.hasClass('selected')) {
+                $this.removeClass('selected');                
+                $this.css({'background-color': 'white'});
+            } else {
+                $this.addClass('selected');                
+                $this.css({'background-color': '#E6E6E6'});
+            }
+        }
+
+        function getSelectedEmails () {
+            return $dialog.find('.selected').map(function () {
+                return $(this).data('email');
+            }).get();
+        }
+
+        function searchable ($searchField, $dataList) {
+            $(document).on("input", $searchField.selector, function(evt) {
+                // Hide all rows, then show any rows containing
+                // a value that matches the search string
+                var searchString = evt.target.value;
+                $dataList.find('div.contactRow').hide()
+                $dataList.find('div.contactRow:contains("' + searchString + '")').show();
+            });
+        }
+    }
+
+    function displayRankedContacts(contacts, $dialog) {
+        // Use the app name to get a client_token, then use
+        // the client_token to post/return contacts
+        var rankedContacts;
+
+        $.ajax({
+            url: YESGRAPH_BASE_URL + '/v0/client-token',
+            data: {appName: appName},
+            success: function (r) {
+                // Use the returned token to POST the contacts
+                // to YesGraph & get ranked results
+                var requestData = contacts;
+                requestData.client_token = r.token;
+                $.ajax({
+                    url: YESGRAPH_BASE_URL + '/v0/address-book',
+                    method: 'POST',
+                    data: JSON.stringify(requestData),
+                    contentType: "application/json; charset=UTF-8",
+                    success: function(r) {
+                        rankedContacts = r.data;
+                    },
+                    error: function(r) {
+                        // If YesGraph ranking errors, fail silently,
+                        // just rendering the unranked contacts
+                        rankedContacts = contacts.entries;
+                    },
+                    complete: function() {
+                        renderContacts(rankedContacts, $dialog);
+                    }
+                });
+            },
+            error: function(r) {
+                // If YesGraph token request errors, fail silently,
+                // just rendering the unranked contacts
+                rankedContacts = contacts.entries;
+                renderContacts(rankedContacts, $dialog);
+            }
+        })
 
         return contacts;
     }
 
-    function getRankedContacts(contacts) {
-        // TODO: get ranked contacts from YesGraph
-        return contacts;
-    }
-
-    $.fn.showContactsList = function (accessToken) {
-        // Use the accessToken to get contacts, and then
-        // display them on the target element.
-        console.log('Loading contacts.');
-
+    $.fn.showContactsList = function (gmailAccessToken) {
+        // Use the gmailAccessToken to get  Gmail contacts,
+        // and then display them on the target element.
         var contacts,
             $targetElem = $(this),
             contactsFeedUrl = 'https://www.google.com/m8/feeds/contacts/default/full',
             readContactsScope = 'https://www.googleapis.com/auth/contacts.readonly';
 
         var queryParams = {
-            access_token: accessToken,
+            access_token: gmailAccessToken,
             alt: 'json',
             orderby: 'lastmodified'
         }
@@ -94,63 +230,94 @@
             dataType: "jsonp",
             success: function (r) {
                 contacts = parseGoogleContactsFeed(r.feed);
-                contacts = getRankedContacts(contacts);
-
-                // Display the ranked
-                var entry,
-                    $contactRow,
-                    $contactsTable = $('<table>'),
-                    $contactsList = $contactsTable.append($('<tbody>'));
-
-                for (i in contacts.entries) {
-                    entry = contacts.entries[i];
-
-                    if (entry.emails) {
-
-                        $contactRow = $('<tr>').css({
-                            display: 'table-row'
-                        });
-                        $contactRow.append($('<td>'))
-                                   .append($('<input>', {type: 'checkbox'}));
-                        $contactRow.append($('<td>', {text: entry.name}));
-                        $contactRow.append($('<td>', {text: entry.emails}));
-
-                        // Style the contacts list
-                        $contactRow.css({
-                            display: 'table-row'
-                        }).children().css({
-                            display: 'table-cell',
-                        })
-                        $contactsTable.append($contactRow);
-                    }
-                }
-                $targetElem.append($contactsTable);
+                displayRankedContacts(contacts, $targetElem);
             },
             error: function(e) {
-                console.log('Error');
-                console.log(e);
+                // FIXME: Fail gracefully if for whatever
+                // reason we can't get the gmail contacts
+                console.log("Google contacts error")
             }
         });
     }
 
-    $.fn.showContactImporter = function () {
-        var $importContactsDiv = $('<div>');
+    $.fn.showContactImportPage = function (options) {
+        // Show the button to import gmail contacts
+        // along with a form to type in email contacts
 
-        // Add a button for importing Google Contacts
+        // TODO: Use custom styling options
+        var $importContactsDiv = $('<div>');
         var $googleContactsBtn = $importContactsDiv.append($('<a>', {
-            text: "Google",
-            href: getGoogleAuthUrl()
-        }));
+            href: getGoogleAuthUrl(options)
+        }).append($('<button>', {
+            text: "Gmail"
+        })));
 
         $(this).append($importContactsDiv);
-    }
 
-    $.fn.showEmailImportForm = function () {
-        var $emailInputDiv = $(this).append($('<div>')),
-            $emailInputForm = $emailInputDiv.append($('<form>')),
-            $emailInput = $emailInputForm.append($('<input>', {type: 'text'})),
-            $addBtn = $emailInputForm.append($('<input>', {type: 'submit', value: 'Add'})),
-            $nextBtn = $emailInputDiv.append($('<button>', {text: 'Next'}));
+        // Manual email input
+        var emails,
+            $emailInputDiv = $(this).append($('<div>')),
+            $emailInputForm = $('<form>'),
+            $inviteesDiv = $('<div>'),
+            $emailInput = $('<input>', {
+                type: 'text',
+                placeholder: 'Email'
+            }),
+            $addBtn = $('<button>', {
+                role: 'submit',
+                text: 'Add'
+            }),
+            $clearBtn = $('<button>', {
+                text: 'Clear'
+            }),
+            $sendBtn = $('<button>', {
+                text: 'Send'
+            });
+
+        $emailInputForm.append($emailInput, $addBtn);
+        $emailInputDiv.append($emailInputForm, $inviteesDiv, $clearBtn, $sendBtn);
+
+        $addBtn.on("click", function(evt) {
+            evt.preventDefault();
+
+            var $invitee = $('<p>', {
+                class: "invitee",
+                text: $emailInput.val()
+            });
+            $inviteesDiv.append($invitee);
+            $emailInput.val("")
+        });
+
+        $clearBtn.on("click", function(evt) {
+            evt.preventDefault();
+            $inviteesDiv.html("");
+        });
+
+        $sendBtn.on("click", function(evt) {
+            evt.preventDefault();
+
+            // Get the input emails & put them in a mailto
+            emails = getSelectedEmails($inviteesDiv).join(',');
+            window.location.href = "mailto:" + emails;
+        });
+
+        // TODO: Allow custom CSS
+        var inputCSS = {
+            "border": "1px solid #CCC",
+            "border-radius": "3px",
+            "margin": "5px",
+            "padding": "0.25em 0.5em"
+        };
+        $emailInputDiv.find('input').css(inputCSS);
+
+        var buttonCSS = inputCSS;
+        buttonCSS["background-color"] = "#F3F3F3";
+        $emailInputDiv.find('button').css(buttonCSS);
+
+        var inviteesDivCSS = inputCSS;
+        inviteesDivCSS["background-color"] = "white";
+        inviteesDivCSS["min-height"] = "100px";
+        $inviteesDiv.css(inviteesDivCSS);
     }
 
     function buildInviteDialog ($dialogBtn) {
@@ -161,14 +328,11 @@
             autoOpen: false,
             modal: true,
             width: 500,
-            buttons: [
-                {
-                    text: "Close",
-                    click: function() {
-                        $( this ).dialog( "close" );
-                    }
+            buttons: {
+                "Close": function() {
+                    $( this ).dialog( "close" );
                 }
-            ]
+            }
         });
 
         $dialogBtn.on('click', function(evt) {
@@ -180,22 +344,57 @@
     }
 
     $.fn.yesgraphInvites = function() {
-        var urlParams = getUrlParams(),
+        appName = $(this).data('app');
+
+        var optionsUrl = YESGRAPH_BASE_URL + '/apps/' + appName + '/js/get-options',
+            tokenValidationUrl = "https://www.googleapis.com/oauth2/v3/tokeninfo",
+            urlParams = getUrlParams(),
+            gmailAccessToken = urlParams.access_token,
+            gmailAccessDenied = urlParams.error,
             $inviteDialog = buildInviteDialog($(this));
 
-        // If we have an access token, get contacts & display them.
-        // Otherwise, load the contact importer instead.
+        $.ajax({
+            dataType: "json",
+            url: optionsUrl,
+            success: function (appOptions) {
+                // Once we have the app's custom options, we should
+                // check for a gmail access token and validate it.
 
-        if ('access_token' in urlParams) {
-            $inviteDialog.dialog("open");
-            $inviteDialog.showContactsList(urlParams.access_token);
+                if (gmailAccessToken) {
+                    // If the token is valid, use it to load contacts.
+                    // Otherwise, clear the token & get a new one.
+                    // https://developers.google.com/identity/protocols/OAuth2UserAgent#validatetoken
 
-        } else {
-            $inviteDialog.showContactImporter();
-            $inviteDialog.showEmailImportForm();
-        }
+                    $.ajax({
+                        url: tokenValidationUrl,
+                        data: {access_token: gmailAccessToken},
+                        success: function (data) {
+                            // Valid token. Pull & display contacts
+                            $inviteDialog.showContactsList(gmailAccessToken);
+                            $inviteDialog.dialog("open");
+                        },
+                        error: function (data) {
+                            // Invalid token.
+                            $inviteDialog.showContactImportPage(appOptions);
+                        }
+                    });
+
+                } else if (gmailAccessDenied) {
+                    // If access was already denied, immediately open
+                    // the contact importer.
+                    $inviteDialog.dialog("open");
+                    $inviteDialog.showContactImportPage(appOptions);
+
+                } else {
+                    // We don't have gmailAccessToken OR gmailAccessDenied,
+                    // so we setup the contact import page but leave the dialog
+                    // closed until the user manually opens it.
+                    $inviteDialog.showContactImportPage(appOptions);
+                }
+            },
+        });
     }
 
-}(jQuery))
+    $('.yesgraph').yesgraphInvites();
 
-$('.yesgraph').yesgraphInvites();
+}(jQuery))
