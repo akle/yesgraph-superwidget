28ff81e05d9c7bfcb7c225d37c04de5710a58e9f
Kendall Chuang <kendallc@gmail.com>
2016-05-28 10:43:30 -0700

    Update to tests, enable methods for testing on yesgraph.js

--- tests/test_api.js
+++ tests/test_api.js
@@ -29,81 +29,104 @@ describe('testAPI', function() {
    */
     
     
-    beforeEach(function () {
-        jasmine.getFixtures().fixturesPath = "base/tests";  // path to your templates
-        jasmine.getFixtures().load('fixtures.html.js');   // load a template
+
+  /*
+    function waitForAPIConfig() {
+        if (window.YesGraphAPI.getApp() && window.YesGraphAPI.hasClientToken() && ($(window.YesGraphAPI.getSettings().target).length > 0)) {
+            done();
+        }
+        else {
+            spyOn(window.YesGraphAPI.storeToken);
+        }
+
+
+
+        //http://stackoverflow.com/questions/12080087/how-to-test-the-done-and-fail-deferred-object-by-using-jasmine
+        var deferred = new jQuery.Deferred();
+        spyOn($, 'ajax').andReturn(deferred);
+        
+        var d = $.Deferred();
+        var timer = setInterval(function() {
+            if (window.YesGraphAPI.getApp() && window.YesGraphAPI.hasClientToken() && ($(window.YesGraphAPI.getSettings().target).length > 0)) {
+
+                clearInterval(timer);
+
+                d.resolve();
+            };
+        }, 100);
+        return d.promise();
+    }
+    */
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+
+    jasmine.getFixtures().fixturesPath = "base/tests";  // path to your templates
+    jasmine.getFixtures().load('fixtures.html.js');   // load a template
+   
+    beforeEach(function (done) {
+        if (window.YesGraphAPI.getApp() && 
+            window.YesGraphAPI.hasClientToken() && 
+            (window.YesGraphAPI.getSettings().target.length > 0)) {
+            done();
+        }
+        else {
+            setTimeout(function() {
+                done();
+            }, 5000);
+        }
     });
     afterEach(function() {
-
-        jasmine.getFixtures().cleanUp();
-        jasmine.getFixtures().clearCache();
+        //jasmine.getFixtures().cleanUp();
+        ///jasmine.getFixtures().clearCache();
     });
 
 
 
     it('Should have yesgraph', function() {
         console.info(window.YesGraphAPI);
-
         expect(window.YesGraphAPI).not.toBe(null);
     });
 
     
     it('Should hit test endpoint', function() {
-
         var result = window.YesGraphAPI.test();
         console.info('result' + result);
         expect(result).not.toBe(null);
-        
+
     });
 
 
-    
     it('Should hit invites accepted endpoint', function() {
         window.YesGraphAPI.postInvitesAccepted = jasmine.createSpy("invitesAcceptedSpy");
         window.YesGraphAPI.postInvitesAccepted();
         expect(window.YesGraphAPI.postInvitesAccepted).toHaveBeenCalled();
     });
 
-    /*
-    it('Should hit API for client token', function() {
-        console.info(window.YesGraphAPI.CLIENT_TOKEN);
-
-
-        window.YesGraphAPI.storeToken = jasmine.createSpy("storeTokenSpy");
-        window.YesGraphAPI.hitAPI = jasmine.createSpy("hitAPISpy");
-        window.YesGraphAPI.getClientToken({});
-        expect(window.YesGraphAPI.hitAPI).toHaveBeenCalled();
-        expect(window.YesGraphAPI.storeToken).toHaveBeenCalled();
-    });
-    */
 
     it('Should store client token', function() {
-        console.info(window.YesGraphAPI.CLIENT_TOKEN);
-        expect(window.YesGraphAPI.CLIENT_TOKEN).not.toBeDefined();
-
-        expect(window.YesGraphAPI.hasClientToken()).toBe(false);
-
-        console.info(window.YesGraphAPI.storeToken);
-        expect(window.YesGraphAPI.storeToken).toBeDefined();
-
-        var token_data = {"inviteLink": "TEST_INVITE_LINK", "token": "CLIENT_TOKEN"};
-        window.YesGraphAPI.storeToken(token_data);
-        expect(window.YesGraphAPI.CLIENT_TOKEN).toBeDefined();
-        
+        expect(window.YesGraphAPI.getClientToken()).toBeDefined();
+        expect(window.YesGraphAPI.hasClientToken()).toBe(true);
     });
 
-    it('Should have variables set', function() {
-
-        console.info(window.YesGraphAPI.YESGRAPH_BASE_URL);
-        console.info(window.YesGraphAPI.YESGRAPH_API_URL);
-        console.info(window.YesGraphAPI.CLIENT_TOKEN_ENDPOINT);
-        console.info(window.YesGraphAPI.ADDRBOOK_ENDPOINT);
-        console.info(window.YesGraphAPI.SUGGESTED_SEEN_ENDPOINT);
-        console.info(window.YesGraphAPI.INVITES_SENT_ENDPOINT);
-        console.info(window.YesGraphAPI.INVITES_ACCEPTED_ENDPOINT);
-        expect(window.YesGraphAPI.YESGRAPH_BASE_URL).toBeDefined();
-        
+    /*
+    it('Should have settings set', function() {
+        console.info(window.YesGraphAPI.settings.getApp());
+        console.info(window.YesGraphAPI.settings.testmode);
+        console.info(window.YesGraphAPI.settings.target);
+        console.info(window.YesGraphAPI.settings.contactImporting);
+        console.info(window.YesGraphAPI.settings.promoteMatchingDomain);
+        console.info(window.YesGraphAPI.settings.emailSending);
+        console.info(window.YesGraphAPI.settings.inviteLink);
+        console.info(window.YesGraphAPI.settings.shareBtns);
+        expect(window.YesGraphAPI.settings.getApp()).toBeDefined();
+        expect(window.YesGraphAPI.settings.testmode).toBeDefined();
+        expect(window.YesGraphAPI.settings.target).toBeDefined();
+        expect(window.YesGraphAPI.settings.contactImporting).toBeDefined();
+        expect(window.YesGraphAPI.settings.promoteMatchingDomain).toBeDefined();
+        expect(window.YesGraphAPI.settings.emailSending).toBeDefined();
+        expect(window.YesGraphAPI.settings.inviteLink).toBeDefined();
+        expect(window.YesGraphAPI.settings.shareBtns).toBeDefined();
     });
+    */
 
 
 });
