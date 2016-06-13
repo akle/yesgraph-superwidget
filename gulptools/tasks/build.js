41ccfe3d1dc194e60d5d4ff0823a156b1aafdc51
Carolyn <carolyn.lee@yale.edu>
2016-06-13 10:49:22 -0700

    Add versioning to build sequence

--- gulptools/tasks/build.js
+++ gulptools/tasks/build.js
@@ -1,7 +1,11 @@
 var gulp = require("gulp");
 var config = require("../config");
 
-gulp.task("build", ["minify"], function(){
+gulp.task("_preBuild", ["minify"], function() {
+    return gulp.start("version")
+});
+
+gulp.task("build", ["_preBuild"], function(){
     return gulp.src(config.tasks.build.files, {base: config.src.root})
         .pipe(gulp.dest(config.dest.root));
 });
