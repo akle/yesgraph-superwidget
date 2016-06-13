26f719bd5856dcf70d0e53c1171ecffaef71484e
Carolyn <carolyn.lee@yale.edu>
2016-06-13 11:25:25 -0700

    Simplify version regex

--- gulptools/tasks/version.js
+++ gulptools/tasks/version.js
@@ -4,34 +4,14 @@ var replace = require("gulp-replace");
 var fs = require("fs");
 var config = require("../config");
 
-var __OLD_SDK_VERSION__ = config.version.sdk;
-var __OLD_SUPERWIDGET_VERSION__ = config.version.superwidget;
-var __OLD_CSS_VERSION__ = config.version.css;
-
-var __NEW_SDK_VERSION__;
-var __NEW_SUPERWIDGET_VERSION__;
-var __NEW_CSS_VERSION__;
-
-function bump(version, updateType) {
-    if (updateType === "NONE") {
-        return version;
-    }
-    var pattern = /^v(\d*).(\d*).(\d*)$/;
-    var updated = (version.match(pattern) || []).slice(1);
-    if (updateType === "PATCH") {
-        updated[2] = String(Number(updated[2]) + 1);
-    } else if (updateType === "MINOR") {
-        updated[1] = String(Number(updated[1]) + 1);
-        updated[2] = "0";
-    } else if (updateType === "MAJOR") {
-        updated[0] = String(Number(updated[0]) + 1);
-        updated[1] = "0";
-        updated[2] = "0";
-    }
-    return "v" + updated.join(".");
+var oldVersions = config.version;
+var newVersions = {
+    sdk: undefined,
+    superwidget: undefined,
+    css: undefined
 }
 
-gulp.task("getVersion", function() {
+gulp.task("_getVersion", function() {
     return gulp.src(config.tasks.version.files, {base: config.src.root})
         .pipe(prompt.prompt([{
             type: 'list',
@@ -51,32 +31,53 @@ gulp.task("getVersion", function() {
             message: 'Which type of bump shoud be used for the CSS?',
             choices: ["NONE", "PATCH", "MINOR", "MAJOR"]
         }], function(res) {
-            __NEW_SDK_VERSION__ = bump(__OLD_SDK_VERSION__, res.sdk_update_type);
-            __NEW_SUPERWIDGET_VERSION__ = bump(__OLD_SUPERWIDGET_VERSION__, res.superwidget_update_type);
-            __NEW_CSS_VERSION__ = bump(__OLD_CSS_VERSION__, res.css_update_type);
-            console.log("SDK Version:", __NEW_SDK_VERSION__);
-            console.log("Superwidget Version:", __NEW_SUPERWIDGET_VERSION__);
-            console.log("CSS Version:", __NEW_CSS_VERSION__);
+            newVersions.sdk = getNewVersion(oldVersions.sdk, res.sdk_update_type)
+            newVersions.superwidget = getNewVersion(oldVersions.superwidget, res.superwidget_update_type)
+            newVersions.css = getNewVersion(oldVersions.css, res.css_update_type)
+            console.log("SDK Version:", newVersions.sdk);
+            console.log("Superwidget Version:", newVersions.superwidget);
+            console.log("CSS Version:", newVersions.css);
         }));
 });
 
-gulp.task("version", ["getVersion"] , function() {
+gulp.task("version", ["_getVersion"] , function() {
     // Update the config file to persist the version change
-    var sdk_re = /(__SDK_VERSION__ ?\= ?[\'\"])(\S*)([\'\"];)/;
-    var superwidget_re = /(__SUPERWIDGET_VERSION__ ?\= ?[\'\"])(\S*)([\'\"];)/;
-    var css_re = /(__CSS_VERSION__ ?\= ?[\'\"])(\S*)([\'\"];)/;
     gulp.src(__dirname + "/../config.js")
-        .pipe(replace(sdk_re, "$1" + __NEW_SDK_VERSION__ + "$3"))
-        .pipe(replace(superwidget_re, "$1" + __NEW_SUPERWIDGET_VERSION__ + "$3"))
-        .pipe(replace(css_re, "$1" + __NEW_CSS_VERSION__ + "$3"))
+        .pipe(replace(/__(\w*)_VERSION__ ?\= ?[\'\"]\S*[\'\"];/, configVersionReplacer))
         .pipe(gulp.dest(__dirname + "/../"));
 
     // Update each of the code files
     var CURRENT_DATE = new Date();
     return gulp.src(config.tasks.version.files, {base: config.src.root})
-        .pipe(replace("__SDK_VERSION__", __NEW_SDK_VERSION__))
-        .pipe(replace("__SUPERWIDGET_VERSION__", __NEW_SUPERWIDGET_VERSION__))
-        .pipe(replace("__CSS_VERSION__", __NEW_CSS_VERSION__))
+        .pipe(replace(/__(\w*)_VERSION__/, fileVersionReplacer))
         .pipe(replace("__BUILD_DATE__", CURRENT_DATE))
         .pipe(gulp.dest(config.dest.root));
 });
+
+function getNewVersion(version, updateType) {
+    if (updateType === "NONE") {
+        return version;
+    }
+    var pattern = /^v(\d*).(\d*).(\d*)$/;
+    var updated = (version.match(pattern) || []).slice(1);
+    if (updateType === "PATCH") {
+        updated[2] = String(Number(updated[2]) + 1);
+    } else if (updateType === "MINOR") {
+        updated[1] = String(Number(updated[1]) + 1);
+        updated[2] = "0";
+    } else if (updateType === "MAJOR") {
+        updated[0] = String(Number(updated[0]) + 1);
+        updated[1] = "0";
+        updated[2] = "0";
+    }
+    return "v" + updated.join(".");
+}
+
+function fileVersionReplacer(match, target) {
+    return newVersions[target.toLowerCase()]
+}
+
+function configVersionReplacer(match, target) {
+    var version = newVersions[target.toLowerCase()]
+    return '__' + target + '_VERSION__ = "' + version + '";';
+}
