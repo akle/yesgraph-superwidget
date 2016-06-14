67b382c6fc8e850c03a59c9050cb19f76bec25a6
Carolyn <carolyn.lee@yale.edu>
2016-06-13 17:01:57 -0700

    Code cleanup

--- gulptools/tasks/deploy.js
+++ gulptools/tasks/deploy.js
@@ -1,19 +1,22 @@
 var gulp = require("gulp");
+var aws = require("gulp-awspublish");
 var clone = require("gulp-clone")
+var cloudfront = require('gulp-cloudfront-invalidate');
 var rename = require("gulp-rename");
 var sequence = require("run-sequence").use(gulp);
+
 var config = require("../config");
-var aws = require("gulp-awspublish");
-var cloudfront = require('gulp-cloudfront-invalidate-aws-publish');
 var publisher = aws.create(config.s3);
 
 gulp.task("deploy", function(done) {
     sequence("clean", "build", function(){
         var cloneSink = clone.sink();
         gulp.src(config.tasks.deploy.files)
+            // We use cloneSink here to create a copy of the files and rename
+            // only the copies (so that they can be sorted by version in S3).
             .pipe(cloneSink)
             .pipe(rename(function(path){
-                // Add versions to the filepath to organize in s3
+                // Add versions to the filepath
                 if (path.basename == "yesgraph-invites" && path.extname == ".css") {
                     path.dirname += "/" + config.version.css;
                 } else if (path.basename == "yesgraph-invites" && path.extname == ".js") {
@@ -22,14 +25,12 @@ gulp.task("deploy", function(done) {
                     path.dirname += "/" + config.version.sdk;
                 }
             }))
+            // For every pipe after cloneSink.tap(), both the originals and
+            // the clones are be used.
             .pipe(cloneSink.tap())
-            .pipe(rename(function(path) {
-                path.dirname = "temp/" + path.dirname; // FIXME - temp for dev
-            }))
             .pipe(publisher.publish({}, {force: true}))
             .pipe(aws.reporter())
             .pipe(cloudfront(config.cloudfront))
-            .pipe(gulp.dest(config.root))
             .on("end", done);
-    })
+    });
 });
