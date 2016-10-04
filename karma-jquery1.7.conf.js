642cdb4b5e961d1f44f5334d756422b1c549f332
Kendall Chuang <kendallc@gmail.com>
2016-10-04 14:44:41 -0700

    One more test for Safari

--- karma-jquery1.7.conf.js
+++ karma-jquery1.7.conf.js
@@ -132,7 +132,7 @@ module.exports = function(config) {
 
     if (process.env.TRAVIS && process.env.TRAVIS_OS_NAME == "osx") {
     // http://swizec.com/blog/how-to-run-javascript-tests-in-chrome-on-travis/swizec/6647
-        config.browsers = ['Chrome_travis_ci', 'Firefox', 'Safari'];
+        config.browsers = ['Firefox', 'Safari'];
         // Which plugins to enable
         config.plugins = [
             'karma-jasmine-jquery',
