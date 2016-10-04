dcb3a206b0bb91118d4036a43ea5c6639f297eae
Kendall Chuang <kendallc@gmail.com>
2016-10-04 16:01:39 -0700

    Updating Firefox -- seems to be a Travis server side issue for not finding the binary

--- karma.conf.js
+++ karma.conf.js
@@ -137,6 +137,7 @@ module.exports = function(config) {
     // http://swizec.com/blog/how-to-run-javascript-tests-in-chrome-on-travis/swizec/6647
         config.browsers = ['Firefox', 'Safari'];
         // Which plugins to enable
+        config.reporters = ['progress'];
         config.plugins = [
             'karma-jasmine-jquery',
             'karma-jasmine',
