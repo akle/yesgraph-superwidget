eb1e274971cda4094c104fd1202d1d2edfbbbdf9
Carolyn <carolyn.lee@yale.edu>
2017-01-09 09:39:41 -0800

    Update version

--- src/modules/view.js
+++ src/modules/view.js
@@ -138,6 +138,7 @@ export default function View() {
 
     this.fetchContactsFailed = function(resp) {
         $(document).trigger(self.Superwidget.YesGraphAPI.events.CONTACT_IMPORT_FAILED, [resp]);
+        self.flashMessage.error(resp.error);
     };
 
     function Modal(options) {
